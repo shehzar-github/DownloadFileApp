18e1919be5311a5da5b429c27319c820e9e03ca1
Shehzar Shakoor <sshakoor@in.ibm.com>
2019-05-12 18:26:33 +0530

    show file download toast message

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,7 @@ import { map,tap,last } from 'rxjs/operators';
 import {BehaviorSubject} from 'rxjs';
 import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer/ngx';
 import { File } from '@ionic-native/file/ngx';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -12,27 +13,34 @@ import { File } from '@ionic-native/file/ngx';
 export class HomePage {
   percentage=0;
   showProgressBar=false;
-  public uploadProgress: BehaviorSubject<number> = new BehaviorSubject<number>(0);
-  public downloadProgress: BehaviorSubject<number> = new BehaviorSubject<number>(0);
   
-  constructor(private http:HttpClient,private transfer: FileTransfer,private file: File){}
+  constructor(private http:HttpClient,private transfer: FileTransfer,private file: File,public toastController: ToastController){}
    fileTransfer: FileTransferObject = this.transfer.create()
   
   downloadFileWithFileTransfer(url){
     this. showProgressBar=true;
-    let downloadUrl = 'https://devdactic.com/html/5-simple-hacks-LBT.pdf';
-    this.fileTransfer.download(downloadUrl, this.file.dataDirectory + 'file.pdf').then((entry) => {
+    //let downloadUrl = 'https://devdactic.com/html/5-simple-hacks-LBT.pdf';
+    this.fileTransfer.download(url, this.file.dataDirectory + 'file.pdf').then((entry) => {
       console.log('download complete: ' + entry.toURL());
+      this.presentToast();
      // this.percentage=0;
      this.showProgressBar=false;
     }, (error) => {
       // handle error
       console.log(error);
     });
-    this.fileTransfer.onProgress((progressEvent) => {
-        let perc = Math.floor(progressEvent.loaded / progressEvent.total * 100);
-        this.percentage = perc/100;
+    // this.fileTransfer.onProgress((progressEvent) => {
+    //     let perc = Math.floor(progressEvent.loaded / progressEvent.total * 100);
+    //     this.percentage = perc/100;
+    // });
+
+  }
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Your file have been downloaded.',
+      duration: 2000
     });
+    toast.present();
   }
   
 }
